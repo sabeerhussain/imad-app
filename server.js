f2facb51e6a9f5ca1d6aa07ce19b49f706a84ef6
sabeerhussain <30728775+sabeerhussain@users.noreply.github.com>
2017-08-09 22:16:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.get('/article-one',function(req, res){
     app.get('/article-two',function(req, res){
      res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('/article-three',function(req, res){
- res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/main.js',function(req, res){
+ res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
